15d00e884bc4910efb115d5c7d7ce1380981012a
Hubert Osipowicz <hubert.osipowicz@wiosna.org.pl>
2022-03-15 00:35:31 +0100

    New section paid box v2

--- template-parts/main-opening-hours.php
+++ template-parts/main-opening-hours.php
@@ -1,18 +1,22 @@
 <?php
-$test = get_field('test');
+$main_open_hours_icon = get_field('main_open_hours_icon');
+$main_open_hours_title = get_field('main_open_hours_title');
+$main_open_hours_cnt = get_field('main_open_hours_cnt');
+$main_open_hours_background = get_field('main_open_hours_background');
 ?>
-<section class="opening-hours py-4 py-lg-5" <?php /*style="background-image: url('<?php echo $test; ?>');"*/ ?> id="opening-hours">
+
+<section class="opening-hours py-4 py-lg-5" style="background-image: url('<?php echo $main_open_hours_background; ?>');" id="opening-hours">
     <div class="container">
         <h2 class="text-center standard-title-3 mb-0">
-            <span class="mea-culpa-font">Godziny otwarcia sklepów</span><br>
-            <span class="oswald-font text-brown-brand fw-bold">STAROPOLSKA MASARNIA</span>
+            <?php echo $main_open_hours_title; ?>
         </h2>
         <div class="sc-title d-flex justify-content-center mb-3 mb-lg-4">
             <div class="sc-title__line">
-                <img width="50" height="50" src="/wp-content/uploads/2022/03/flower-decor-2.png" class="" alt="">
+                <img width="50" height="50" src="<?php echo $main_open_hours_icon['url']; ?>" class="img-fluid mx-auto" alt="<?php echo $main_open_hours_icon['alt']; ?>">
             </div>
         </div>
-        <div class="row">
+        <?php echo $main_open_hours_cnt; ?>
+        <!-- <div class="row">
             <div class="col-lg-6 d-flex flex-column">
                 <div class="opening-hours__box p-3 flex-grow-1">
                     <h4 class="standard-title-6 oswald-font text-brown-brand">PŁOCK:</h4>
@@ -56,6 +60,6 @@ $test = get_field('test');
                     </ul>
                 </div>
             </div>
-        </div>
+        </div> -->
     </div>
 </section>
