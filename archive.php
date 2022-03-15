84bfb6dd10752345a2624f87972f43b3a627b52d
Hubert Osipowicz <hubert.osipowicz@wiosna.org.pl>
2022-03-15 15:52:49 +0100

    New section paid box v3

--- archive.php
+++ archive.php
@@ -12,15 +12,58 @@ get_header();
 
 $name = get_the_archive_title();
 $name_cat = strtolower(preg_replace('/-+/', '-', preg_replace('/[^\wáéíóú]/', '-', $name)));
+
+if (in_category('wedzonki')) {
+    $body_background = get_field('tlo', 1108);
+    $top_header_image = get_field('top_header_image', 1108);
+    $top_header_icon = get_field('top_header_icon', 1108);
+} else if (in_category('szynki')) {
+    $body_background = get_field('tlo', 1083);
+    $top_header_image = get_field('top_header_image', 1083);
+    $top_header_icon = get_field('top_header_icon', 1083);
+} else if (in_category('suche-i-podsuszane')) {
+    $body_background = get_field('tlo', 1108);
+    $top_header_image = get_field('top_header_image', 1108);
+    $top_header_icon = get_field('top_header_icon', 1108);
+} else if (in_category('pasztety')) {
+    $body_background = get_field('tlo', 1108);
+    $top_header_image = get_field('top_header_image', 1108);
+    $top_header_icon = get_field('top_header_icon', 1108);
+} else if (in_category('pasztety')) {
+    $body_background = get_field('tlo', 1108);
+    $top_header_image = get_field('top_header_image', 1108);
+    $top_header_icon = get_field('top_header_icon', 1108);
+} else if (in_category('kielbasy-grube')) {
+    $body_background = get_field('tlo', 1108);
+    $top_header_image = get_field('top_header_image', 1108);
+    $top_header_icon = get_field('top_header_icon', 1108);
+} else if (in_category('kielbasy-cienkie')) {
+    $body_background = get_field('tlo', 1108);
+    $top_header_image = get_field('top_header_image', 1108);
+    $top_header_icon = get_field('top_header_icon', 1108);
+} else if (in_category('garmazerka')) {
+    $body_background = get_field('tlo', 1108);
+    $top_header_image = get_field('top_header_image', 1108);
+    $top_header_icon = get_field('top_header_icon', 1108);
+} else {
+    $body_background = get_field('tlo');
+}
+
 ?>
 
-<div class="page-header-top page-header-top-<?php echo $name_cat; ?> mb-3 mb-lg-5 d-flex flex-column align-items-center justify-content-center">
-    <h1 class="page-header-top__title text-white text-center">
-        <?php the_archive_title(); ?>
+<div class="page-header-top page-header-top-<?php echo $name_cat; ?> d-flex flex-column align-items-center justify-content-center" style="background-image: url('<?php echo $top_header_image; ?>');">
+    <h1 class="page-header-top__title text-white text-center standard-title-3 mb-0">
+        <span class="mea-culpa-font"><?php the_archive_title(); ?></span>
+        <br><span class="oswald-font text-brown-brand fw-bold">STAROPOLSKA MASARNIA</span>
     </h1>
+    <div class="sc-title d-flex justify-content-center mb-3 mb-lg-4">
+        <div class="sc-title__line">
+            <img width="50" height="50" src="<?php echo $top_header_icon['url']; ?>" class="img-fluid mx-auto" alt="<?php echo $top_header_icon['alt']; ?>">
+        </div>
+    </div>
 </div>
 
-<main id="main" class="[- products-boxes mb-4 mb-lg-5 -]" role="main">
+<main id="main" class="products-boxes py-4 py-lg-5" role="main" style="background-image: url('<?php echo $body_background; ?>');">
     <div class="container">
         <div id="products-boxes" class="row">
             <?php
@@ -34,9 +77,9 @@ $name_cat = strtolower(preg_replace('/-+/', '-', preg_replace('/[^\wáéíóú]/
                                 $image  = wp_get_attachment_image_src($imgID, 'large', false, '');
                                 $imgAlt = get_post_meta($imgID, '_wp_attachment_image_alt', true);
                                 ?>
-                                <img src="<?php echo $image[0]; ?>" alt="<?php echo $imgAlt; ?>" class="img-fluid" style="border-radius: 20px;border: 1px solid rgba(119, 71, 2, .2);">
+                                <img src="<?php echo $image[0]; ?>" alt="<?php echo $imgAlt; ?>" class="img-fluid" style="border: 1px solid rgba(119, 71, 2, .2);">
                             <?php else : ?>
-                                <img src="<?php bloginfo('template_directory'); ?>/images/produkty-masarnia-staropolska.jpg" class="img-fluid" style="border-radius: 20px;border: 1px solid rgba(119, 71, 2, .2);">
+                                <img src="<?php bloginfo('template_directory'); ?>/images/produkty-masarnia-staropolska.jpg" class="img-fluid" style="border: 1px solid rgba(119, 71, 2, .2);">
                             <?php endif; ?>
                         </figure>
                         <!-- Product title -->
@@ -46,10 +89,10 @@ $name_cat = strtolower(preg_replace('/-+/', '-', preg_replace('/[^\wáéíóú]/
                         <!-- product desc slide content -->
                         <div class="product__content slide-up-content p-3 p-lg-4">
                             <div class="slide-up-content__content">
-                                <h3 class="products-boxes__title mb-3 mb-lg-4">
+                                <h3 class="products-boxes__title mb-3 mb-lg-4 oswald-font standard-title-5">
                                     <strong><?php the_title(); ?></strong>
                                 </h3>
-                                <div class="overflow-auto"><?php the_content(); ?></div>
+                                <div class="overflow-auto fw-light"><?php the_content(); ?></div>
                             </div>
                             <span class="slide-up-content__close close-content">
                                 <span class="close-button"></span>
@@ -73,30 +116,6 @@ $name_cat = strtolower(preg_replace('/-+/', '-', preg_replace('/[^\wáéíóú]/
     </div>
 </main>
 <?php get_footer(); ?>
-
-
-
-<style>
-    .products-boxes .product__content {
-        display: flex;
-        flex-direction: column;
-        justify-content: space-between;
-        background-color: #774702;
-        color: #fff;
-        padding: 35px 25px 15px;
-        position: absolute;
-        top: 100%;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        transition: .3s ease-in-out;
-        border-radius: 20px;
-    }
-
-    .products-boxes .product__content.active {
-        top: 0;
-    }
-</style>
 
 <script>
     jQuery('.see-more-content').on('click', function() {
